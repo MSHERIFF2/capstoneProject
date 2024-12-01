3b527bfd333aa5df114d056af1755e1477c6e255
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-12-01 12:26:43 +0000

    updated

--- GradeKeeper/scripts.js
+++ GradeKeeper/scripts.js
@@ -1,42 +1,55 @@
+const studentTable = document.getElementById('student-data');
+const addStudentForm = document.getElementById('add-student-form');
+const api = 'http://localhost:3000/api/students'
 // Get all students
-fetch('/api/students')
+fetch(api)
   .then(response => response.json())
   .then(data => {
-    // Render student data in the table
     data.forEach(student => {
       const row = `<tr>
         <td>${student.name}</td>
         <td>${student.class}</td>
         <td>${student.subject}</td>
-        <td>${student.ca.join(', ')}</td>
-        <td>${student.exam.join(', ')}</td>
+        <td>${student.ca}</td>
+        <td>${student.exam}</td>
         <td>${calculateGrade(student.ca, student.exam)}</td>
       </tr>`;
       studentTable.innerHTML += row;
     });
   });
 
+  function calculateGrade(ca, exam) {
+    const total = ca + exam;
+    const average = total / 2;
+    return average.toFixed(2);
+  }
 // Add a new student
 addStudentForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  console.log("I am click");
   const formData = new FormData(addStudentForm);
   const studentData = Object.fromEntries(formData);
-  fetch('/api/students', {
+  fetch(api, {
     method: 'POST',
     body: JSON.stringify(studentData),
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json())
-    .then(data => {
-      // Render the new student data in the table
+  .then(response => response.json())
+  .then(data => {
+    c
+    if (data && data.ca && data.exam) {
       const row = `<tr>
         <td>${data.name}</td>
         <td>${data.class}</td>
         <td>${data.subject}</td>
         <td>${data.ca}</td>
         <td>${data.exam}</td>
-        <td>${calculateGrade(data.ca, data.exam)}</td>
+        <td>${calculateGrade(parseFloat(data.ca), parseFloat(data.exam))}</td>
       </tr>`;
       studentTable.innerHTML += row;
-    });
+    } else {
+      console.error("Invalid data:", data);
+    }
+  });
 });
+
--- GradeKeeper/server.js
+++ GradeKeeper/server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3000;
 
 app.use(express.json());
-
+app.use(cors())
 // Load student data from the JSON file
 const students = require('./students.json');
 
 // API endpoint to get all students
+
 app.get('/api/students', (req, res) => {
-  res.json(students);
+  res.json(students.map(student => ({...student, ca: student.ca, exam: student.exam })));
 });
 
 // API endpoint to add a new student
 app.post('/api/students', (req, res) => {
   const newStudent = req.body;
   students.push(newStudent);
-  res.json(newStudent);
+  res.json({...student, ca: newStudent.ca, exam: newStudent.exam });
 });
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
